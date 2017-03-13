e306716029159c298b36092a462d85cf1b971ce8
Garrick <gcheung@fitbit.com>
2017-03-13 08:35:56 -0700

    Correct method name.

--- addon/components/vega-vis.js
+++ addon/components/vega-vis.js
@@ -91,7 +91,7 @@ export default Ember.Component.extend({
     },
 
     willDestroyElement() {
-        this._teardownResize();
+        this._teardownWindowResize();
 
         let vis = this.get('vis');
 
